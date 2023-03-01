b2c1ae3640e677ed7db41322211cadcad06e1f74
enrique moreno <enrique.moreno80302@gmail.com>
2023-02-28 20:43:33 -0600

    hi

--- Demo-3/script.js
+++ Demo-3/script.js
@@ -4,15 +4,11 @@ const cardFront = document.querySelector('.card-front');
 const cardBack = document.querySelector('.card-back');
 const createCardButton = document.querySelector('.create-card-button');
 
-
 // Add event listener to flip button
 const flipButton = document.querySelector('.flip-button');
 flipButton.addEventListener('click', function() {
   cardContainer.classList.toggle('flip');
 });
-
-
-
 
 // Add event listener to form submission
 const form = cardBack.querySelector('form');
@@ -53,6 +49,20 @@ cardContainer.addEventListener('mousemove', function(e) {
   }
 });
 
+const colorButtons = document.querySelectorAll(".color-button");
+colorButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    const color = this.getAttribute("data-color");
+    const cardContainer = this.parentNode.parentNode;
+    const cardBack = cardContainer.querySelector(".card-back");
+    cardContainer.style.backgroundColor = color;
+    document.querySelector('.card-front').style.backgroundColor = color;
+  });
+});
+
+
+
+
 createCardButton.addEventListener('click', () => {
   const newCardContainer = document.createElement('div');
   newCardContainer.classList.add('card-container');
@@ -68,13 +78,19 @@ createCardButton.addEventListener('click', () => {
   newCardBack.classList.add('card', 'card-back');
   newCardBack.innerHTML = '<form><label for="summary">Summary:</label><input type="text" id="summary" name="summary" placeholder="Enter summary..."><label for="name">Name:</label><input type="text" id="name" name="name" placeholder="Enter name..."><label for="date">Date:</label><input type="text" id="date" name="date" placeholder="Enter date..."><button type="submit">Save</button></form>';
 
+  //Create color buttons for the new card
+  const newColorButtons = document.createElement('div');
+  newColorButtons.classList.add('color-buttons');
+  newColorButtons.innerHTML = '<button class="color-button" data-color="red">Red</button><button class="color-button" data-color="blue">Blue</button><button class="color-button" data-color="green">Green</button>';
+
+  newCardBack.appendChild(newColorButtons);
   newCardContainer.appendChild(newCard);
   newCardContainer.appendChild(newCardBack);
 
   cardContainer.parentNode.insertBefore(newCardContainer, cardContainer.nextSibling);
 
-   // Add event listeners to the new card container
-   newCardContainer.addEventListener('mousedown', function(e) {
+  // Add event listeners to the new card container
+  newCardContainer.addEventListener('mousedown', function(e) {
     startX = e.clientX;
     startY = e.clientY;
     isDragging = true;
@@ -95,4 +111,36 @@ createCardButton.addEventListener('click', () => {
       newCardContainer.style.top = `${initialY + deltaY}px`;
     }
   });
+
+  // Add event listener to form submission for new card
+  const newForm = newCardBack.querySelector('form');
+  newForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const summary = newForm.elements.summary.value;
+    const name = newForm.elements.name.value;
+    const date = newForm.elements.date.value;
+    const frontText = `Summary: ${summary}<br>Name: ${name}<br>Date: ${date}`;
+    newCard.querySelector('p').innerHTML = frontText;
+    newCardContainer.classList.remove('flip'); // flip the card back to the front
+  });
+
+  // Add event listener to flip button for new card
+  const newFlipButton = newCard.querySelector('.flip-button');
+  newFlipButton.addEventListener('click', function() {
+    newCardContainer.classList.toggle('flip');
+  });
+
+  // Add event listeners to the new color buttons
+  const newColor = newCardBack.querySelector(".color-buttons");
+newColor.addEventListener("click", function(event) {
+  if (event.target.classList.contains("color-button")) {
+    const color = event.target.getAttribute("data-color");
+    newCard.style.backgroundColor = color;
+    newCardBack.style.backgroundColor = color;
+  }
 });
+});
+
+  
+
+
